0eb2358ccb8c6ccef79e9a0f837269a14a827440
phigasui <phigasui@gmail.com>
2018-03-11 18:39:15 +0900

    update: remove cache for www

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 aws s3 sync . s3://phigasui.com/ --exclude '*' --include '*.html' --include '*.jpg' --include '*.png' --include '*.svg' --include '*.css' --include '*.js' --include '*.ico' --include '*.json'
 aws cloudfront create-invalidation --distribution-id E1G18L83B2NRRD --paths "/*"
+aws cloudfront create-invalidation --distribution-id E34YI2ZZELSP2N --paths "/*"
